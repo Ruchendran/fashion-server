f6d916a5f5544def9e46fb8d3b9b90f8702fab97
Chathruknan Sathvathu <vvruchendran141594@gmail.com>
2025-10-29 14:19:27 +0530

    mongodb atlas

--- db.js
+++ db.js
@@ -1,25 +1,25 @@
-const mongoose=require("mongoose");
-const getStartDb=(async ()=>{
-    mongoose.connect('mongodb://localhost:27017/products').then(()=>{
-        console.log("DB connected");
-    }).catch((error)=>{
-        console.log(`DB error: ${error}`);
-    })
-});
-module.exports={getStartDb}; 
-
-
-
-
 // const mongoose=require("mongoose");
 // const getStartDb=(async ()=>{
-//     mongoose.connect('mongodb+srv://chathruknasathvathu_db_user:CJUhJFLbNph6BM0z@cluster0.awuza5b.mongodb.net/',{
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     }).then(()=>{
+//     mongoose.connect('mongodb://localhost:27017/products').then(()=>{
 //         console.log("DB connected");
 //     }).catch((error)=>{
 //         console.log(`DB error: ${error}`);
 //     })
 // });
 // module.exports={getStartDb}; 
+
+
+
+
+const mongoose=require("mongoose");
+const getStartDb=(async ()=>{
+    mongoose.connect('mongodb+srv://chathruknasathvathu_db_user:CJUhJFLbNph6BM0z@cluster0.awuza5b.mongodb.net/',{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(()=>{
+        console.log("DB connected");
+    }).catch((error)=>{
+        console.log(`DB error: ${error}`);
+    })
+});
+module.exports={getStartDb}; 
