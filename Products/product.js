220aba3da7855139c45d2622df4041216d0384e5
Chathruknan Sathvathu <vvruchendran141594@gmail.com>
2026-02-28 18:47:59 +0530

    i

--- Products/product.js
+++ Products/product.js
@@ -3,29 +3,6 @@ const productRoute=express.Router();
 const path=require("path");
 const productModel=require("../Models/productModel.js");
 const orderModel=require("../Models/orderModel.js");
-productRoute.get("/totalRecords/:productFamily",async(req,res,next)=>{
-    let totalRecords;
-    let group=req.params.productFamily.slice(0,1).toUpperCase()+req.params.productFamily.slice(1,req.params.productFamily.length);
-    if(group == 'All'){
-        totalRecords=await productModel.find({})
-    }
-    else{
-        totalRecords=await productModel.find({productFamily:group});
-    }
-    res.status(200).send({totalRecords:totalRecords.length})
-})
-// productRoute.get("/filterPrice",async(req,res,next)=>{
-//     let allProducts;
-//     let group=req.query.productFamily.slice(0,1).toUpperCase()+req.query.productFamily.slice(1,req.query.productFamily.length);
-//     if(group == 'All'){
-//        allProducts =await productModel.find({productPrice:{$lte:req.query.maxPrice ,$gte:req.query.minPrice}});
-//     }
-//     else{
-//         allProducts=await productModel.find({productPrice:{$lte:req.query.maxPrice ,$gte:req.query.minPrice},productFamily:group});
-//     }
-//     res.status(200).send(allProducts);
-// })
-
 productRoute.put("/upd-product/feedback",async(req,res,next)=>{
     try{
         const feedbackProductList=req.body.productsListPayload;
@@ -63,6 +40,30 @@ productRoute.put("/upd-product/feedback",async(req,res,next)=>{
         res.status(404).send({message:e.message+'fsf'});
     }
 })
+productRoute.get("/totalRecords/:productFamily",async(req,res,next)=>{
+    let totalRecords;
+    let group=req.params.productFamily.slice(0,1).toUpperCase()+req.params.productFamily.slice(1,req.params.productFamily.length);
+    if(group == 'All'){
+        totalRecords=await productModel.find({})
+    }
+    else{
+        totalRecords=await productModel.find({productFamily:group});
+    }
+    res.status(200).send({totalRecords:totalRecords.length})
+})
+// productRoute.get("/filterPrice",async(req,res,next)=>{
+//     let allProducts;
+//     let group=req.query.productFamily.slice(0,1).toUpperCase()+req.query.productFamily.slice(1,req.query.productFamily.length);
+//     if(group == 'All'){
+//        allProducts =await productModel.find({productPrice:{$lte:req.query.maxPrice ,$gte:req.query.minPrice}});
+//     }
+//     else{
+//         allProducts=await productModel.find({productPrice:{$lte:req.query.maxPrice ,$gte:req.query.minPrice},productFamily:group});
+//     }
+//     res.status(200).send(allProducts);
+// })
+
+
 ///It is aways last.
 productRoute.get("/:productFamily/:page",async(req,res,next)=>{
     let allProducts;
