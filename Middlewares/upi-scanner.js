aad9ddbbb51268d8fb7cc795b71c7c1f9e5d680e
Chathruknan Sathvathu <vvruchendran141594@gmail.com>
2026-07-25 13:32:18 +0530

    latest

--- Middlewares/upi-scanner.js
+++ Middlewares/upi-scanner.js
@@ -41,10 +41,10 @@ const scanPyment=async(req,res,next)=>{
     // payment wrong
     else{
         if(orderTotalAmount > payAmount){
-             res.status(400).send({message:`U have sent incorrect payment. ur total order amount is ${orderTotalAmount} but u sent oly ${payAmount}.this amount is pending ${orderTotalAmount - payAmount}`});
+             res.status(400).send({message:"U sent less than your order amount"});
         }
         else{
-            res.status(400).send({message:`u have more than orderAmount.ur order amount is ${orderTotalAmount} but u sent ${payAmount}.this much amount is more ${payAmount-orderTotalAmount}`})
+            res.status(400).send({message:"u sent moe than ur order amount"})
         }
     }
     worker.terminate();
